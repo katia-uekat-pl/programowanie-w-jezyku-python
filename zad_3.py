049081ed3caa268224d802836ea7587bf9f3fe22
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-18 12:10:56 +0100

    zad_3

---
+++ zad_3.py
@@ -0,0 +1,10 @@
+def if_even_number(number: int) -> bool:
+    return number % 2 == 0
+
+the_number = 1000
+the_outcome = if_even_number(the_number)
+
+if the_outcome:
+    print("Even number")
+else:
+    print("Odd numer")
