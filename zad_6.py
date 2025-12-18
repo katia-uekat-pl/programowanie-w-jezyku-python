495a9d3fb5416c82faf614bb0470727c02c4ec11
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-18 14:28:33 +0100

    zad_6

---
+++ zad_6.py
@@ -0,0 +1,21 @@
+def list_operations(list1:list, list2:list)->list:
+
+    unique_elements = set(list1  + list2)
+
+    return [x**3 for x in unique_elements]
+
+print(list_operations([1,1,4],[1,9,1]))
+
+#Alternative approach with for each:
+def list_operations(list1:list, list2:list)->list:
+
+    unique_elements = set(list1  + list2)
+    outcome_list = []
+
+    for element in unique_elements:
+        given_power = element ** 3
+        outcome_list.append(given_power)
+
+    return outcome_list
+
+print(list_operations([1,1,4],[1,9,1]))
