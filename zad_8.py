d511d59e638dcefe5b4c298d921fa8a63c881ff8
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-26 00:21:09 +0100

    zad_8 improved

--- zad_8.py
+++ zad_8.py
@@ -1,46 +1,76 @@
 from typing import List, Optional
 import requests
 import argparse
+
 url_API = "https://api.openbrewerydb.org/v1/breweries"
+
 
 class Brewery:
     def __init__(
-        self,
-        name: str,
-        brewery_type: str,
-        city: str,
-        country: str,
-        **kwargs
+            self,
+            name: str,
+            brewery_type: str,
+            city: str,
+            country: str,
+            **kwargs
     ):
         self.name = name
         self.brewery_type = brewery_type
         self.city = city
         self.country = country
-        self.id = id
+        self.id = kwargs.get('id')
 
     def __str__(self) -> str:
         return (
             f"BROWAR: {self.name}\n"
-            f"TYPE:    {self.brewery_type}\n"
-            f"CITY: {self.city} ({self.country})\n"
+            f"TYPE:   {self.brewery_type}\n"
+            f"CITY:   {self.city} ({self.country})\n"
+            f"ID:     {self.id}\n"
             f"{'=' * 30}"
         )
 
 
 def get_breweries_from_api(city: str | None) -> list:
-    limit_param = "per_page=20"
 
-    if city is not None:
+    params = {"per_page": 20}
+    if city:
+        params["by_city"] = city
 
-        url = f'{url_API}?by_city={city}&{limit_param}'
-        return requests.get(url).json()
-
-    return requests.get(f'{url_API}?{limit_param}').json()
+    try:
+        response = requests.get(url_API, params=params)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Błąd pobierania: {e}")
+        return []
 
 
 def brewery_factory(breweries_data: list) -> List[Brewery]:
-
     brewery_objects = []
     for item in breweries_data:
         brewery_objects.append(Brewery(**item))
     return brewery_objects
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Browary API')
+    parser.add_argument('-c', '--city', help='Filtruj po mieście', required=False)
+    return vars(parser.parse_args())
+
+
+def main():
+    args = get_args()
+    # Pobieramy dane
+    raw_data = get_breweries_from_api(city=args.get('city'))
+    # Tworzymy obiekty
+    breweries = brewery_factory(raw_data)
+
+    # Wyświetlamy
+    for b in breweries:
+        print(b)
+
+    print(f"Pobrano: {len(breweries)} obiektów.")
+
+
+if __name__ == "__main__":
+    main()
