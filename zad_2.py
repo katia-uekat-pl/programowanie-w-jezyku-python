701666ef98cf1a34caa92921ec08984cc0b54afa
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-26 23:12:38 +0100

    zad_2 (PEP8 adjustment)

--- zad_2.py
+++ zad_2.py
@@ -1,5 +1,8 @@
 def multiply_numbers(number1: int, number2: int):
     return number1 * number2
 
-multiply_result=multiply_numbers(10,10)
+
+multiply_result = multiply_numbers(10, 10)
+
+
 print(multiply_result)
