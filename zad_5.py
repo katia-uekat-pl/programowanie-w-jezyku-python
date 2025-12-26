c5c67bab03f9ecefd493e4a0b52d013614964f7e
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-26 23:57:46 +0100

    zad_5 (PEP8 adjustment)

--- zad_5.py
+++ zad_5.py
@@ -1,5 +1,8 @@
-def if_value1_contains_value2(value1:list, value2: int) -> bool:
+def if_value1_contains_value2(value1: list, value2: int) -> bool:
     return value2 in value1
 
-the_outcome = (if_value1_contains_value2([2],2))
+
+the_outcome = if_value1_contains_value2([2], 2)
+
+
 print(the_outcome)
