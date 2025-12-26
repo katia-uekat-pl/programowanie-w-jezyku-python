b60548de96ab189aba204d203712fd116a368e7c
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-26 23:46:23 +0100

    zad_4 (PEP8 adjustment)

--- zad_4.py
+++ zad_4.py
@@ -1,25 +1,32 @@
-#First approach:
+# " First approach:
 
-def first_two_numbers_bigger_than_third (number1: int, number2: int, number3: int) -> bool:
+def first_two_numbers_bigger_than_third(
+        number1: int, number2: int, number3: int) -> bool:
     return number1 + number2 >= number3
 
-the_outcome = first_two_numbers_bigger_than_third(5,5,5)
+
+the_outcome = first_two_numbers_bigger_than_third(5, 5, 5)
 print(the_outcome)
 
-#Second approach with the list:
+# "Second approach with the list:
 
-def first_two_numbers_bigger_than_third (number1: int, number2: int, number3: int) -> bool:
+
+def first_two_numbers_bigger_than_third(
+        number1: int, number2: int, number3: int) -> bool:
     return number1 + number2 >= number3
 
-the_numbers = [5,5,5]
+
+the_numbers = [5, 5, 5]
 
 the_outcome = first_two_numbers_bigger_than_third(*the_numbers)
 print(the_outcome)
 
-#for the logging purpose:
-a,b,c = the_numbers
+# "for the logging purpose:"
+a, b, c = the_numbers
 
-if  the_outcome:
-    print(f"The sum of the [{a}] and [{b}] is: [{a+b}], which is bigger or equal to [{c}].")
+if the_outcome:
+    print(f"The sum of the [{a}] and [{b}] is: [{a+b}], "
+          f"which is bigger or equal to [{c}].")
 else:
-    print(f"The sum of [{a}] and [{b}] is p{a+b}], which is less than [{c}].")
+    print(f"The sum of [{a}] and [{b}] is p{a+b}], "
+          f"which is less than [{c}].")
