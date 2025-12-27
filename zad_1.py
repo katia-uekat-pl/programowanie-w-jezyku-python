cffb93ae29793fe2c5830f65bed53b43cc53b9d7
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-27 13:04:44 +0100

    zad_1.py (PEP8 adjustment)

--- zad_1.py
+++ zad_1.py
@@ -1,17 +1,17 @@
 class Student:
-    def __init__(self, name: str, marks:list[int]):
+    def __init__(self, name: str, marks: list[int]):
         self.name = name
         self.marks = marks
 
-    def is_passed(self)-> bool:
+    def is_passed(self) -> bool:
         if not self.marks:
             return False
-        average= sum(self.marks) / len(self.marks)
+        average = sum(self.marks) / len(self.marks)
         return average > 50
 
 
-student_passed = Student("Person1", [70,60,80])
-student_failed = Student("Person2",[5,8,9])
+student_passed = Student("Person1", [70, 60, 80])
+student_failed = Student("Person2", [5, 8, 9])
 
 print(f" Has {student_passed.name} paseed? {student_passed.is_passed()}")
 print(f" Has {student_failed.name} paseed? {student_failed.is_passed()}")
