20d1d6d117a153c4096d8f0f41a53da1e08fa585
katia-uekat.pl <katarine.gonchar@gmail.com>
2025-12-28 19:51:16 +0100

    added division by zero handling

--- zad_1.py
+++ zad_1.py
@@ -6,6 +6,8 @@ class Student:
     def is_passed(self) -> bool:
         if not self.marks:
             return False
+        if len(self.marks) == 0:
+            return False
         average = sum(self.marks) / len(self.marks)
         return average > 50
 
@@ -13,5 +15,5 @@ class Student:
 student_passed = Student("Person1", [70, 60, 80])
 student_failed = Student("Person2", [5, 8, 9])
 
-print(f" Has {student_passed.name} paseed? {student_passed.is_passed()}")
-print(f" Has {student_failed.name} paseed? {student_failed.is_passed()}")
+print(f" Has {student_passed.name} passed? {student_passed.is_passed()}")
+print(f" Has {student_failed.name} passed? {student_failed.is_passed()}")
